550eef5aaa66a7a0e7db223296ed5ae1ee047f4c
Marius Eismantas <gmmarius@gmail.com>
2018-05-25 20:49:05 +0300

    Initial item commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RSP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
